809d90aedbdace555d3aef31ff4346ef3ed43e32
maximofn <maximofn@gmail.com>
2022-09-07 16:11:29 +0200

    Se borran los archivos de test y se meten los posts y el theme

---
+++ posts/modulo.py
@@ -0,0 +1,9 @@
+
+def funcion1_del_modulo(nombre):
+  print("Hola, " + nombre + ", funcion 1")
+
+def funcion2_del_modulo(nombre):
+  print("Hola, " + nombre + ", funcion 2")
+
+def funcion3_del_modulo(nombre):
+  print("Hola, " + nombre + ", funcion 3")

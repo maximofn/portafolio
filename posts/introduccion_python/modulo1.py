992cb78c0276f309075d70e64448bdda1ecf91df
maximofn <maximofn@gmail.com>
2023-12-16 03:15:44 +0100

    Elimino archivos que no hace falta que estén en el repo

--- posts/introduccion_python/modulo1.py
+++
@@ -1,3 +0,0 @@
-
-def funcion_del_modulo(nombre):
-  print("Hola, " + nombre)
--- posts/introduccion_python/modulo2.py
+++
@@ -1,9 +0,0 @@
-
-def funcion1_del_modulo(nombre):
-  print("Hola, " + nombre + ", funcion 1")
-
-def funcion2_del_modulo(nombre):
-  print("Hola, " + nombre + ", funcion 2")
-
-def funcion3_del_modulo(nombre):
-  print("Hola, " + nombre + ", funcion 3")

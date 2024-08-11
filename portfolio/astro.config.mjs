ed9384261145b2c0ec47a56e7517b7ccdf796274
maximofn <maximofn@gmail.com>
2024-08-11 20:37:38 -0300

    Cambio el idioma por defecto en la configuración de astro

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     i18n: {
-      defaultLocale: "en",
-      locales: ["es", "en", "pt-br", "pt-pt"],
+      defaultLocale: "es",
+      locales: ["es", "en", "pt-br"],
     }
   })

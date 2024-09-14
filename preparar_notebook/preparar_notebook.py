c4fa2669972eb6be3508e3163717452f1bf0d711
maximofn <maximofn@gmail.com>
2024-09-14 09:52:33 +0200

    Empiezo con la corrección ortográfica del notebook

--- preparar_notebook/preparar_notebook.py
+++ preparar_notebook/preparar_notebook.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
 from get_notebook_metadata import get_notebook_metadata, check_if_notebook_metadata_is_ok
+from preprocess_jupyter_notebook import correct_notebook
+from error_codes import QUOTA_EXCEEDED_ERROR
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Preparar notebook')
@@ -16,7 +18,15 @@ if __name__ == "__main__":
 
     # Check if metadata is ok
     if check_if_notebook_metadata_is_ok(notebook_metadata):
-        print("Metadata is ok")
+        print("Metadata is ok\n")
     else:
         print("Metadata is not ok")
         exit(1)
+    
+    # Corrections from gemini
+    notebook_correctios_dict = correct_notebook(notebook_path)
+    if notebook_correctios_dict and notebook_correctios_dict != QUOTA_EXCEEDED_ERROR:
+        print("Notebook corrections:")
+        print(notebook_correctios_dict)
+    elif notebook_correctios_dict == QUOTA_EXCEEDED_ERROR:
+        print("Quota exceeded error")

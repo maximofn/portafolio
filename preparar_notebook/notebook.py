574338de3f05c35ddeddbae86d794b8af2d4af56
maximofn <maximofn@gmail.com>
2024-09-20 05:27:57 +0200

    Completo la clase notebook

--- preparar_notebook/notebook.py
+++ preparar_notebook/notebook.py
@@ -7,3 +7,25 @@ class Notebook:
     def get_content_as_json(self):
         with open(self.path, 'r') as f:
             return json.load(f)
+    
+    def cells(self):
+        content = self.get_content_as_json()
+        return content['cells']
+    
+    def number_cells(self):
+        return len(self.cells())
+    
+    def markdown_cells(self):
+        return [cell for cell in self.cells() if cell['cell_type'] == 'markdown']
+
+    def number_markdown_cells(self):
+        return len(self.markdown_cells())
+    
+    def save_content_dict(self, content):
+        with open(self.path, 'w') as file:
+            json.dump(content, file, indent=2)
+        
+    def save_cells(self, cells):
+        content = self.get_content_as_json()
+        content['cells'] = cells
+        self.save_content_dict(content)

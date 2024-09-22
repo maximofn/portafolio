c302cdb37a1a2288662f5a242a48d5e1fb6b5bd6
maximofn <maximofn@gmail.com>
2024-09-22 04:36:38 +0200

    Se actualiza la clase notebook

--- preparar_notebook/notebook.py
+++ preparar_notebook/notebook.py
@@ -1,19 +1,29 @@
 import json
+import copy
 
 class Notebook:
-    def __init__(self, path, end_name=None):
+    def __init__(self, path, add_path=None, end_name=None):
         self.path = path
         self.content = None
+        if add_path:
+            self.path = self.path.parent / f"{add_path}" / f"{self.path.stem}{self.path.suffix}"
         if end_name:
             self.path = self.path.parent / f"{self.path.stem}_{end_name}{self.path.suffix}"
 
     def get_content_as_json(self):
-        with open(self.path, 'r') as f:
-            return json.load(f)
+        if self.content:
+            return self.content
+        else:
+            with open(self.path, 'r') as f:
+                self.content = json.load(f)
+                return self.content
     
     def cells(self):
         content = self.get_content_as_json()
         return content['cells']
+    
+    def copy_from(self, notebook):
+        self.content = copy.deepcopy(notebook.content.copy())
     
     def number_cells(self):
         return len(self.cells())

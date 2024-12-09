d1fdcd4278c6f423a4e847ac09719094dc71717d
maximofn <maximofn@gmail.com>
2024-12-09 21:18:14 +0100

    test(tests): :dizzy: Add test for fix of correction jupyter notebooks

--- preparar_notebook/tests/test_corrections_jupyter_notebook.py
+++ preparar_notebook/tests/test_corrections_jupyter_notebook.py
@@ -29,3 +29,59 @@ class Test_corrections_jupyter_notebook(unittest.TestCase):
         line = "La capital de España es Madrid, pero no es la capital de Francia. Sin embargo, la capital de Francia es larís."
         result = apply_corrections(self.model, line)
         self.assertEqual(result, "La capital de España es Madrid, pero no es la capital de Francia. Sin embargo, la capital de Francia es París.")
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_empty_line(self, mock_stdout, mock_input):
+        line = ""
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, line)
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_space_line(self, mock_stdout, mock_input):
+        line = " "
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, line)
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_tab_line(self, mock_stdout, mock_input):
+        line = "\t"
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, line)
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_return_line(self, mock_stdout, mock_input):
+        line = "\n"
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, line)
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_example1(self, mock_stdout, mock_input):
+        line = "Vamos a probar ahora esta función de embeddings en local"
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, "Vamos a probar ahora esta función de embeddings en local")
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_example2(self, mock_stdout, mock_input):
+        line = "Para hacer todas estas pruebas me he descargado el dataset [aws-case-studies-and-blogs](https://www.kaggle.com/datasets/harshsinghal/aws-case-studies-and-blogs) y lo he dejado en la carpeta `rag-txt_dataset`, con los siguientes comandos te digo cómo descargarlo y descomprimirlo"
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, "Para hacer todas estas pruebas me he descargado el dataset [aws-case-studies-and-blogs](https://www.kaggle.com/datasets/harshsinghal/aws-case-studies-and-blogs) y lo he dejado en la carpeta `rag-txt_dataset`, con los siguientes comandos te digo cómo descargarlo y descomprimirlo")
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_example3(self, mock_stdout, mock_input):
+        line = "A crear los `chunk`s!"
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, "A crear los `chunk`s!")
+
+    @patch('builtins.input', return_value='y')   # Mock the input to avoid asking for input
+    @patch('sys.stdout')    # Mock the stdout to avoid printing to the console
+    def test_apply_corrections_example4(self, mock_stdout, mock_input):
+        line = "Volvemos a recordar, este modelo de embeddings es muy pequeño, solo 22M de parámetros, por lo que casi en cualquier ordenador se puede ejecutar, más rápido o más lento, pero se puede."
+        result = apply_corrections(self.model, line)
+        self.assertEqual(result, "Volvemos a recordar, este modelo de embeddings es muy pequeño, solo 22M de parámetros, por lo que casi en cualquier ordenador se puede ejecutar, más rápido o más lento, pero se puede ejecutar.")

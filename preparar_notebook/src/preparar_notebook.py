22c485d5954c3e5367f5c3bbef5293f27cd827f8
maximofn <maximofn@gmail.com>
2024-12-28 13:45:17 +0100

    feat(preparar_notebook): :rocket: add add_page_to... to preparar_notebook script

--- preparar_notebook/src/preparar_notebook.py
+++ preparar_notebook/src/preparar_notebook.py
@@ -5,6 +5,8 @@ from corrections_jupyter_notebook import ortografic_corrections_jupyter_notebook
 from translate_jupyter_notebooks import translate_jupyter_notebook
 from error_codes import QUOTA_EXCEEDED_ERROR
 from jupyter_notebook_to_html import convert_to_html
+from add_page_to_its_json_file import add_page_to_its_json_file_from_metadata
+from add_page_to_its_sitemap import add_page_to_its_sitemap_from_metadata
 import json
 from utils import ask_for_something
 
@@ -14,6 +16,9 @@ def parse_args():
     parser.add_argument('--no_check_metadata', action='store_true', help='Do not check if metadata is ok')
     parser.add_argument('--no_correct_ortografic_errors', action='store_true', help='Do not correct ortografic errors')
     parser.add_argument('--no_translate', action='store_true', help='Do not translate the notebook')
+    parser.add_argument('--no_convert_to_html', action='store_true', help='Do not convert the notebook to html')
+    parser.add_argument('--no_add_to_json', action='store_true', help='Do not add the astro metadata to the json file')
+    parser.add_argument('--no_add_to_sitemap', action='store_true', help='Do not add the astro metadata to the sitemap file')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -22,6 +27,9 @@ if __name__ == "__main__":
     no_check_metadata = args.no_check_metadata
     no_correct_ortografic_errors = args.no_correct_ortografic_errors
     no_translate = args.no_translate
+    no_convert_to_html = args.no_convert_to_html
+    no_add_to_json = args.no_add_to_json
+    no_add_to_sitemap = args.no_add_to_sitemap
 
     # Get metadata from notebook
     notebook_metadata = get_notebook_metadata(notebook_path)
@@ -47,6 +55,18 @@ if __name__ == "__main__":
             translate_jupyter_notebook(notebook_path)
     
     # Convert to HTML
-    print("\nConverting to HTML")
-    notebook_title = notebook_metadata[3]
-    convert_to_html(notebook_path, notebook_metadata, notebook_title)
+    if not no_convert_to_html:
+        if ask_for_something("\nDo you want to convert the notebook to html? (y/n)", ['y', 'yes'], ['n', 'no']):
+            print("\nConverting to HTML")
+            notebook_title = notebook_metadata[3]
+            convert_to_html(notebook_path, notebook_metadata, notebook_title)
+
+    # Add to json
+    if not no_add_to_json:
+        if ask_for_something("\nDo you want to add the astro metadata to the json file? (y/n)", ['y', 'yes'], ['n', 'no']):
+            add_page_to_its_json_file_from_metadata(notebook_metadata, notebook_path)
+
+    # Add to sitemap
+    if not no_add_to_sitemap:
+        if ask_for_something("\nDo you want to add the astro metadata to the sitemap file? (y/n)", ['y', 'yes'], ['n', 'no']):
+            add_page_to_its_sitemap_from_metadata(notebook_metadata, notebook_path)

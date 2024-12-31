d9bc7ee04090d0db9cf3a83bf89a9961bc976479
maximofn <maximofn@gmail.com>
2024-12-31 07:03:56 +0100

    feat(preparar_notebook): :rocket: Update Gemini to 2.0

--- preparar_notebook/src/gemini.py
+++ preparar_notebook/src/gemini.py
@@ -3,11 +3,18 @@ import os
 import google.generativeai as genai
 from error_codes import QUOTA_EXCEEDED_ERROR, INTERNAL_ERROR, API_KEY_ERROR
 
+GEMINI_MODEL_NAME = "gemini-2.0-flash-exp"
+
 class Gemini:
-    def __init__(self, system_instruction):
+    def __init__(self, system_instruction, temperature=1, top_p=0.95, top_k=40, max_output_tokens=8192, response_mime_type="text/plain"):
         self.model = None
-        self.gemini_model_name = "gemini-1.5-pro"
+        self.gemini_model_name = GEMINI_MODEL_NAME
         self.load_gemini_api_key()
+        self.temperature = temperature
+        self.top_p = top_p
+        self.top_k = top_k
+        self.max_output_tokens = max_output_tokens
+        self.response_mime_type = response_mime_type
         self.create_model(system_instruction)
     
     def load_gemini_api_key(self):
@@ -18,9 +25,17 @@ class Gemini:
         genai.configure(api_key=GEMINI_API_KEY)
 
     def create_model(self, system_instruction):
+        generation_config = {
+            "temperature": self.temperature,
+            "top_p": self.top_p,
+            "top_k": self.top_k,
+            "max_output_tokens": self.max_output_tokens,
+            "response_mime_type": self.response_mime_type,
+        }
         self.model = genai.GenerativeModel(
             model_name=self.gemini_model_name,
             system_instruction=system_instruction,
+            generation_config=generation_config
         )
     
     def chat(self, input_text):

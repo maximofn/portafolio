28d15509354d5c0b42137d59fbf2b87725b2f7df
maximofn <maximofn@gmail.com>
2025-05-28 20:11:14 +0200

    Crear el servidor MCP

--- posts/MCP_github/github_server.py
+++ posts/MCP_github/github_server.py
@@ -1,3 +1,10 @@
 from mcp.server.fastmcp import FastMCP
 
+
+# Create an MCP server
 mcp = FastMCP("GitHubMCP")
+
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport='stdio')
